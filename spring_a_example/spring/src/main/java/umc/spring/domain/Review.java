7179c2b6062aeca176ed04a4daade5a06e8c91e3
heejae <hjy10045@sookmyung.ac.kr>
2024-06-03 20:41:40 +0900

    feat: mission/week7 파일 재생성

---
+++ spring_a_example/spring/src/main/java/umc/spring/domain/Review.java
@@ -0,0 +1,33 @@
+package umc.spring.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import umc.spring.domain.common.BaseEntity;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Review extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 50)
+    private String title;
+
+    @Lob
+    private String body;
+
+    @Column(nullable = false)
+    private Float score;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "store_id")
+    private Store store;
+}
