c8864b10666b0dd8c4a3756dde357e80583ae510
meli <melitina915@gmail.com>
2024-06-17 20:38:47 +0900

    [Update(멜리/윤채은)] 9주차 수정
    [Springboot] Chapter 9. API & Swagger & Annotation

---
+++ meli/spring/src/main/java/umc/spring/service/MemberMissionService/MemberMissionQueryServiceImpl.java
@@ -0,0 +1,23 @@
+package umc.spring.service.MemberMissionService;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import umc.spring.domain.enums.MissionStatus;
+import umc.spring.repository.MemberMissionRepository;
+
+// 9주차 4. 가게의 미션을 도전 중인 미션에 추가(미션 도전하기) API
+// - MemberMission을 추가하는 것이 목적
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class MemberMissionQueryServiceImpl implements MemberMissionQueryService {
+
+    private final MemberMissionRepository memberMissionRepository;
+
+    @Override
+    public boolean findMemberMission(Long id, MissionStatus missionStatus){
+        return memberMissionRepository.existsByMissionIdAndStatus(id, MissionStatus.CHALLENGING);
+    }
+
+}
--- meli/spring/src/main/java/umc/spring/validation/validator/MissionChallengingValidator.java
+++ meli/spring/src/main/java/umc/spring/validation/validator/MissionChallengingValidator.java
@@ -6,18 +6,17 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import umc.spring.apiPayload.code.status.ErrorStatus;
 import umc.spring.domain.enums.MissionStatus;
-import umc.spring.repository.MemberMissionRepository;
+import umc.spring.service.MemberMissionService.MemberMissionQueryService;
 import umc.spring.validation.annotation.ChallengingMission;
 
 // 9주차 4. 가게의 미션을 도전 중인 미션에 추가(미션 도전하기) API
 // - MemberMission을 추가하는 것이 목적
 @Component
 @RequiredArgsConstructor
-//public class MissionChallengingValidator implements ConstraintValidator<ChallengingMission, List<Long>> {
 public class MissionChallengingValidator implements ConstraintValidator<ChallengingMission, Long> {
 
-    //private final MissionRepository missionRepository;
-    private final MemberMissionRepository memberMissionRepository;
+    //private final MemberMissionRepository memberMissionRepository;
+    private final MemberMissionQueryService memberMissionQueryService;
 
     @Override
     public void initialize(ChallengingMission constraintAnnotation){
@@ -25,20 +24,9 @@ public class MissionChallengingValidator implements ConstraintValidator<Challeng
     }
 
     @Override
-//    public boolean isValid(List<Long> values, ConstraintValidatorContext context){
-//        boolean isValid = values.stream()
-//                .allMatch(value -> missionRepository.existsById(value));
-//
-//        if(!isValid){
-//            context.disableDefaultConstraintViolation();
-//            context.buildConstraintViolationWithTemplate(ErrorStatus.MISSION_NOT_FOUND.toString()).addConstraintViolation();
-//        }
-//
-//        return isValid;
-//
-//    }
     public boolean isValid(Long missionId, ConstraintValidatorContext context){
-        boolean isChallenging = memberMissionRepository.existsByMissionIdAndStatus(missionId, MissionStatus.CHALLENGING);
+        //boolean isChallenging = memberMissionRepository.existsByMissionIdAndStatus(missionId, MissionStatus.CHALLENGING);
+        boolean isChallenging = memberMissionQueryService.findMemberMission(missionId, MissionStatus.CHALLENGING);
 
         if(isChallenging){
             context.disableDefaultConstraintViolation();
